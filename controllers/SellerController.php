aef3ab6eca2dfd820431fc3729c031af39a0e35a
juanjopareja <jupg25@msn.com>
2023-10-16 18:26:16 +0200

    Add sellers controller

---
+++ controllers/SellerController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Controllers;
+
+class SellerController {
+    public static function create() {
+        echo "Crear vendedor";
+    }
+
+    public static function update() {
+        echo "Actualizar vendedor";
+    }
+
+    public static function delete() {
+        echo "Eliminar vendedor";
+    }
+}
