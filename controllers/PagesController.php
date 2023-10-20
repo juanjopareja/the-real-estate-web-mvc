14b31e23bb4ad6cd0c49b0d871ab222916776b12
juanjopareja <jupg25@msn.com>
2023-10-20 16:51:27 +0200

    Add pages URL's

---
+++ controllers/PagesController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Controllers;
+
+class PagesController {
+    public static function index() {
+        echo "desde el index";
+    }
+
+    public static function about() {
+        echo "desde nosotros";
+    }
+
+    public static function properties() {
+        echo "desde propiedades";
+    }
+
+    public static function property() {
+        echo "desde propiedad";
+    }
+
+    public static function blog() {
+        echo "desde blog";
+    }
+
+    public static function entry() {
+        echo "desde entrada";
+    }
+
+    public static function contact() {
+        echo "desde contacto";
+    }
+}
--- public/index.php
+++ public/index.php
@@ -5,9 +5,11 @@ require_once __DIR__ . '/../includes/app.php';
 use MVC\Router;
 use Controllers\PropertyController;
 use Controllers\SellerController;
+use Controllers\PagesController;
 
 $router = new Router();
 
+// Private area
 $router->get('/admin', [PropertyController::class, 'index']);
 $router->get('/properties/create', [PropertyController::class, 'create']);
 $router->post('/properties/create', [PropertyController::class, 'create']);
@@ -20,5 +22,15 @@ $router->post('/sellers/create', [SellerController::class, 'create']);
 $router->get('/sellers/update', [SellerController::class, 'update']);
 $router->post('/sellers/update', [SellerController::class, 'update']);
 $router->post('/sellers/delete', [SellerController::class, 'delete']);
+
+// Public area
+$router->get('/', [PagesController::class, 'index']);
+$router->get('/about', [PagesController::class, 'about']);
+$router->get('/properties', [PagesController::class, 'properties']);
+$router->get('/property', [PagesController::class, 'property']);
+$router->get('/blog', [PagesController::class, 'blog']);
+$router->get('/entry', [PagesController::class, 'entry']);
+$router->get('/contact', [PagesController::class, 'contact']);
+$router->post('/contact', [PagesController::class, 'contact']);
 
 $router->checkRoutes();
